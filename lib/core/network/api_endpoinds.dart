f318cd2d02019e179416a2607dee1e71570846f8
arif <mdarifhosain214@gmail.com>
2026-05-22 00:38:41 +0600

    api base url and key added

---
+++ lib/core/network/api_endpoinds.dart
@@ -0,0 +1,5 @@
+class APIEndpoinds {
+  static const String baseURL = 'https://www.googleapis.com/books/v1';
+  static const String volumes = '/volumes';
+  static const String key = 'AIzaSyBidj7oSDqmRMM3lLZmck-TekswB_cmi-k';
+}
