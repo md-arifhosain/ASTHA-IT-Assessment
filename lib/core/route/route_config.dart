44dd546791cc9bcdf7b15f3b6a5742ad254b2fff
mdarifhosain214 <mdarifhosain214@gmail.com>
2026-05-22 17:49:04 +0600

    route update book details arguments passed

--- lib/core/route/route_config.dart
+++ lib/core/route/route_config.dart
@@ -1,20 +1,21 @@
 import 'package:astha_it_assessment/core/route/route_name.dart';
+import 'package:astha_it_assessment/presentation/book/book_details/view/book_details_screen.dart';
 import 'package:flutter/material.dart';
 
-
+import '../../data/models/book_list_model.dart';
 import '../../presentation/book/book_list/view/book_list_screen.dart';
 import '../../presentation/splash/view/splash_screen.dart';
-
-
 
 class RouteGenerator {
   static Route<dynamic> getRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
       case RoutesName.splashRoute:
         return MaterialPageRoute(builder: (_) => const SplashScreen());
-         case RoutesName.bookListScreen:
+      case RoutesName.bookListScreen:
         return MaterialPageRoute(builder: (_) => const BookListScreen());
-   
+      case RoutesName.bookDetailsScreen:
+       final book = routeSettings.arguments as BookModel;
+        return MaterialPageRoute(builder: (_) =>  BookDetailsScreen(book:book ,));
 
       default:
         return unDefineRoute();
--- lib/core/route/route_name.dart
+++ lib/core/route/route_name.dart
@@ -2,7 +2,5 @@ class RoutesName {
   static const String splashRoute = "/";
   static const String bookListScreen = "/bookListScreen";
   static const String bookDetailsScreen = "/bookDetailsScreen";
-  static const String registerRoute = "/register";
-  static const String forgotPasswordRoute = "/forgotPassword";
-  static const String bottomNavRoute = "/bottomNav";
+  
 }
