12fc751256c0a98fd4a4c7b3ef25128436efe8ff
mdarifhosain214 <mdarifhosain214@gmail.com>
2026-05-22 11:45:05 +0600

    favourite icon added and updated some file

--- lib/core/route/route_config.dart
+++ lib/core/route/route_config.dart
@@ -2,6 +2,7 @@ import 'package:astha_it_assessment/core/route/route_name.dart';
 import 'package:flutter/material.dart';
 
 
+import '../../presentation/book/book_list/view/book_list_screen.dart';
 import '../../presentation/splash/view/splash_screen.dart';
 
 
@@ -11,6 +12,8 @@ class RouteGenerator {
     switch (routeSettings.name) {
       case RoutesName.splashRoute:
         return MaterialPageRoute(builder: (_) => const SplashScreen());
+         case RoutesName.bookListScreen:
+        return MaterialPageRoute(builder: (_) => const BookListScreen());
    
 
       default:
--- lib/core/route/route_name.dart
+++ lib/core/route/route_name.dart
@@ -1,7 +1,7 @@
 class RoutesName {
   static const String splashRoute = "/";
-  static const String onBoadingRoute = "/onBoading";
-  static const String loginRoute = "/login";
+  static const String bookListScreen = "/bookListScreen";
+  static const String bookDetailsScreen = "/bookDetailsScreen";
   static const String registerRoute = "/register";
   static const String forgotPasswordRoute = "/forgotPassword";
   static const String bottomNavRoute = "/bottomNav";
