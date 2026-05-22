8a0f6d96e268ff3e62d7064351ca9f7d0e131363
mdarifhosain214 <mdarifhosain214@gmail.com>
2026-05-22 17:49:30 +0600

    color manager added

---
+++ lib/core/resources/color_manager.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  ColorManager._();
+  static const Color primaryColor = Color(0xFF4F46E5);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color purpleWhiteColor = Color(0xFFF6F0FF);
+  
+}
